2b3bed58d963406e7fe09c552c77d66988bd5c98
ElSayed ElAshmawy <alexmaster_2004@hotmail.com>
2017-06-20 23:14:20 +0400

    Adding test cases.

---
+++ Xebia.RetailStoreAssignment/Xebia.RetailStore.Tests/Data.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xebia.RetailStore.Tests
+{
+    static class Data
+    {
+        public static List<Product> Products
+        {
+            get;
+            private set;
+        }
+
+        public static List<User> Users
+        {
+            get;
+            private set;
+        }
+
+        static Data()
+        {
+            Products = GetProductsCatalog();
+            Users = Getusers();
+        }
+
+        private static List<Product> GetProductsCatalog()
+        {
+            var productsCat = new List<Product>
+            {
+                new Product
+                {
+                    Name = "P1",
+                    ProductType = ProductType.Other,
+                    Price = 100
+                },
+                new Product
+                {
+                    Name = "P2",
+                    ProductType = ProductType.Grocery,
+                    Price = 100
+                },
+                new Product
+                {
+                    Name = "P3",
+                    ProductType = ProductType.Grocery,
+                    Price = 100
+                },
+                new Product
+                {
+                    Name = "P4",
+                    ProductType = ProductType.Other,
+                    Price = 100
+                },
+                new Product
+                {
+                    Name = "P5",
+                    ProductType = ProductType.Other,
+                    Price = 100
+                },
+                new Product
+                {
+                    Name = "P6",
+                    ProductType = ProductType.Grocery,
+                    Price = 100
+                },
+                new Product
+                {
+                    Name = "P7",
+                    ProductType = ProductType.Other,
+                    Price = 100
+                },
+                new Product
+                {
+                    Name = "P8",
+                    ProductType = ProductType.Grocery,
+                    Price = 100
+                },
+                new Product
+                {
+                    Name = "P9",
+                    ProductType = ProductType.Other,
+                    Price = 100
+                },
+                new Product
+                {
+                    Name = "P10",
+                    ProductType = ProductType.Other,
+                    Price = 100
+                },
+                new Product
+                {
+                    Name = "P11",
+                    ProductType = ProductType.Other,
+                    Price = 100
+                },
+                new Product
+                {
+                    Name = "P12",
+                    ProductType = ProductType.Other,
+                    Price = 100
+                },
+                new Product
+                {
+                    Name = "P13",
+                    ProductType = ProductType.Grocery,
+                    Price = 100
+                },
+                new Product
+                {
+                    Name = "P14",
+                    ProductType = ProductType.Grocery,
+                    Price = 100
+                },
+                new Product
+                {
+                    Name = "P15",
+                    ProductType = ProductType.Other,
+                    Price = 100
+                },
+                new Product
+                {
+                    Name = "P16",
+                    ProductType = ProductType.Other,
+                    Price = 100
+                },
+                new Product
+                {
+                    Name = "P17",
+                    ProductType = ProductType.Other,
+                    Price = 100
+                },
+                new Product
+                {
+                    Name = "P18",
+                    ProductType = ProductType.Other,
+                    Price = 100
+                },
+                new Product
+                {
+                    Name = "P19",
+                    ProductType = ProductType.Other,
+                    Price = 100
+                },
+                new Product
+                {
+                    Name = "P20",
+                    ProductType = ProductType.Other,
+                    Price = 100
+                }
+            };
+
+            return productsCat;
+        }
+
+        private static List<User> Getusers()
+        {
+            var users = new List<User>
+            {
+                new User
+                {
+                    Name = "U1",
+                    IsEmployee = true,
+                    IsAffiliate = true,
+                    JoiningDate = DateTime.UtcNow.AddMonths(-12)
+                },
+                new User
+                {
+                    Name = "U2",
+                    IsEmployee = false,
+                    IsAffiliate = true,
+                    JoiningDate = DateTime.UtcNow.AddMonths(-18)
+                },
+                new User
+                {
+                    Name = "U3",
+                    IsEmployee = false,
+                    IsAffiliate = false,
+                    JoiningDate = DateTime.UtcNow.AddMonths(-12)
+                },
+                new User
+                {
+                    Name = "U4",
+                    IsEmployee = false,
+                    IsAffiliate = false,
+                    JoiningDate = DateTime.UtcNow.AddMonths(-24)
+                },
+                new User
+                {
+                    Name = "U5",
+                    IsEmployee = false,
+                    IsAffiliate = false,
+                    JoiningDate = DateTime.UtcNow.AddMonths(-24).AddDays(-1)
+                },
+                new User
+                {
+                    Name = "U6",
+                    IsEmployee = false,
+                    IsAffiliate = false,
+                    JoiningDate = DateTime.UtcNow.AddMonths(-24).AddDays(1)
+                },
+                new User
+                {
+                    Name = "U7",
+                    IsEmployee = false,
+                    IsAffiliate = false,
+                    JoiningDate = DateTime.UtcNow.AddYears(-3)
+                }
+            };
+
+            return users;
+        }
+    }
+}
---
+++ Xebia.RetailStoreAssignment/Xebia.RetailStore.Tests/RetailStoreTest.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Xebia.RetailStore.Tests
+{
+    public class RetailStoreTest
+    {
+        [Fact]
+        public void UserIsEmployeeWithMixOfProducts()
+        {
+            var bill = new Bill
+            {
+                Customer = Data.Users[0],
+                OrderDetails = new Order
+                {
+                    OrderItems = new List<Product>
+                    {
+                       Data.Products[0],
+                       Data.Products[1],
+                       Data.Products[2],
+                       Data.Products[3],
+                       Data.Products[4]
+                    }
+                }
+            };
+
+            Assert.Equal(bill.NetPayable, 390);
+        }
+
+        [Fact]
+        public void UserIsEmployeeWithGroceriesOnly()
+        {
+            var bill = new Bill
+            {
+                Customer = Data.Users[0],
+                OrderDetails = new Order
+                {
+                    OrderItems = new List<Product>
+                    {
+                       Data.Products[1],
+                       Data.Products[2]
+                    }
+                }
+            };
+
+            Assert.Equal(bill.NetPayable, 190);
+        }
+
+        [Fact]
+        public void UserIsEmployeeWithNoGroceries()
+        {
+            var bill = new Bill
+            {
+                Customer = Data.Users[0],
+                OrderDetails = new Order
+                {
+                    OrderItems = new List<Product>
+                    {
+                       Data.Products[0],
+                       Data.Products[3],
+                       Data.Products[4]
+                    }
+                }
+            };
+
+            Assert.Equal(bill.NetPayable, 200);
+        }
+
+        [Fact]
+        public void UserIsAffiliateWithMixOfProducts()
+        {
+            var bill = new Bill
+            {
+                Customer = Data.Users[1],
+                OrderDetails = new Order
+                {
+                    OrderItems = new List<Product>
+                    {
+                       Data.Products[0],
+                       Data.Products[1],
+                       Data.Products[2],
+                       Data.Products[3],
+                       Data.Products[4]
+                    }
+                }
+            };
+
+            Assert.Equal(bill.NetPayable, 450);
+        }
+
+        [Fact]
+        public void UserIsAffiliateWithGroceriesOnly()
+        {
+            var bill = new Bill
+            {
+                Customer = Data.Users[1],
+                OrderDetails = new Order
+                {
+                    OrderItems = new List<Product>
+                    {
+                       Data.Products[1],
+                       Data.Products[2]
+                    }
+                }
+            };
+
+            Assert.Equal(bill.NetPayable, 190);
+        }
+
+        [Fact]
+        public void UserIsAffiliateWithNoGroceries()
+        {
+            var bill = new Bill
+            {
+                Customer = Data.Users[1],
+                OrderDetails = new Order
+                {
+                    OrderItems = new List<Product>
+                    {
+                       Data.Products[0],
+                       Data.Products[3],
+                       Data.Products[4]
+                    }
+                }
+            };
+
+            Assert.Equal(bill.NetPayable, 260);
+        }
+
+        [Fact]
+        public void UserIsCustomerForLessThanTwoYearsWithMixOfProducts()
+        {
+            var bill = new Bill
+            {
+                Customer = Data.Users[2],
+                OrderDetails = new Order
+                {
+                    OrderItems = new List<Product>
+                    {
+                       Data.Products[0],
+                       Data.Products[1],
+                       Data.Products[2],
+                       Data.Products[3],
+                       Data.Products[4]
+                    }
+                }
+            };
+
+            Assert.Equal(bill.NetPayable, 475);
+        }
+
+        [Fact]
+        public void UserIsCustomerForLessThanTwoYearsWithGroceriesOnly()
+        {
+            var bill = new Bill
+            {
+                Customer = Data.Users[2],
+                OrderDetails = new Order
+                {
+                    OrderItems = new List<Product>
+                    {
+                       Data.Products[1],
+                       Data.Products[2]
+                    }
+                }
+            };
+
+            Assert.Equal(bill.NetPayable, 190);
+        }
+
+        [Fact]
+        public void UserIsCustomerForLessThanTwoYearsWithNoGroceries()
+        {
+            var bill = new Bill
+            {
+                Customer = Data.Users[2],
+                OrderDetails = new Order
+                {
+                    OrderItems = new List<Product>
+                    {
+                       Data.Products[0],
+                       Data.Products[3],
+                       Data.Products[4]
+                    }
+                }
+            };
+
+            Assert.Equal(bill.NetPayable, 285);
+        }
+
+        [Fact]
+        public void UserIsCustomerForJustTwoYearsWithMixOfProducts()
+        {
+            var bill = new Bill
+            {
+                Customer = Data.Users[5],
+                OrderDetails = new Order
+                {
+                    OrderItems = new List<Product>
+                    {
+                       Data.Products[0],
+                       Data.Products[1],
+                       Data.Products[2],
+                       Data.Products[3],
+                       Data.Products[4]
+                    }
+                }
+            };
+
+            Assert.Equal(bill.NetPayable, 475);
+        }
+
+        [Fact]
+        public void UserIsCustomerForJustTwoYearsWithGroceriesOnly()
+        {
+            var bill = new Bill
+            {
+                Customer = Data.Users[5],
+                OrderDetails = new Order
+                {
+                    OrderItems = new List<Product>
+                    {
+                       Data.Products[1],
+                       Data.Products[2]
+                    }
+                }
+            };
+
+            Assert.Equal(bill.NetPayable, 190);
+        }
+
+        [Fact]
+        public void UserIsCustomerForJustTwoYearsWithNoGroceries()
+        {
+            var bill = new Bill
+            {
+                Customer = Data.Users[5],
+                OrderDetails = new Order
+                {
+                    OrderItems = new List<Product>
+                    {
+                       Data.Products[0],
+                       Data.Products[3],
+                       Data.Products[4]
+                    }
+                }
+            };
+
+            Assert.Equal(bill.NetPayable, 285);
+        }
+
+        [Fact]
+        public void UserIsCustomerForMoreThanTwoYearsWithMixOfProducts()
+        {
+            var bill = new Bill
+            {
+                Customer = Data.Users[6],
+                OrderDetails = new Order
+                {
+                    OrderItems = new List<Product>
+                    {
+                       Data.Products[0],
+                       Data.Products[1],
+                       Data.Products[2],
+                       Data.Products[3],
+                       Data.Products[4]
+                    }
+                }
+            };
+
+            Assert.Equal(bill.NetPayable, 465);
+        }
+
+        [Fact]
+        public void UserIsCustomerForMoreThanTwoYearsWithGroceriesOnly()
+        {
+            var bill = new Bill
+            {
+                Customer = Data.Users[6],
+                OrderDetails = new Order
+                {
+                    OrderItems = new List<Product>
+                    {
+                       Data.Products[1],
+                       Data.Products[2]
+                    }
+                }
+            };
+
+            Assert.Equal(bill.NetPayable, 190);
+        }
+
+        [Fact]
+        public void UserIsCustomerForMoreThanTwoYearsWithNoGroceries()
+        {
+            var bill = new Bill
+            {
+                Customer = Data.Users[6],
+                OrderDetails = new Order
+                {
+                    OrderItems = new List<Product>
+                    {
+                       Data.Products[0],
+                       Data.Products[3],
+                       Data.Products[4]
+                    }
+                }
+            };
+
+            Assert.Equal(bill.NetPayable, 275);
+        }
+    }
+}
